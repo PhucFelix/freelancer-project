846ea1991ed5e470fdfcba3d659b64931ad77f98
tranquanglongplusplus <tranquanglongplusplus@gmail.com>
2024-04-21 21:29:57 +0700

    add-default-query

---
+++ database-query/init-user.sql
@@ -0,0 +1,4 @@
+USE spring_security_cooperate;
+INSERT INTO users(id, password, user_name) VALUES (1, '$2a$12$V5sBty4mvHQ503JjOmBiE.b37Ddi8M3rxIDiPt6YNURbQ0ztXyONu', 'longtq');
+INSERT INTO roles(id, name) VALUES (1, 'USER');
+INSERT INTO user_role(user_id, role_id) VALUES (1,1);
